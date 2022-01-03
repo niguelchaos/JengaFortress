78a2e378e1f1fdeeaa84cb6224e2233ded16dab1
Ibrahnab <ibrahim.naboulsi97@gmail.com>
2022-01-03 17:12:09 +0100

    switch phases

--- Assets/Scripts/PlayUI.cs
+++ Assets/Scripts/PlayUI.cs
@@ -11,14 +11,38 @@ public class PlayUI : MonoBehaviour
     public TMP_Text currentPlayerText;
     public Button endTurnButton;
     public Image endTurnButtonImage;
+    public Canvas oldCanvas;
+    public Canvas startScreen;
+    public Canvas placeGroundScreen;
+    public Canvas adjustFireScreen;
+    public Canvas placePlayer1Screen;
+    public Canvas placePlayer2Screen;
+    public Canvas playingScreen;
     //private Player player;
 
     void Start()
     {
         endTurnButton = GameObject.Find("EndTurnButton").GetComponent<Button>();
         endTurnButtonImage = GameObject.Find("EndTurnButton").GetComponent<Image>();
+        currentPlayerText = GameObject.Find("CurrentPlayerText").GetComponent<TMP_Text>();
+
+        oldCanvas = GameObject.Find("Canvas").GetComponent<Canvas>();
+        startScreen = GameObject.Find("startScreen").GetComponent<Canvas>();
+        placeGroundScreen = GameObject.Find("placeGroundScreen").GetComponent<Canvas>();
+        adjustFireScreen = GameObject.Find("adjustFireScreen").GetComponent<Canvas>();
+        placePlayer1Screen = GameObject.Find("placePlayer1").GetComponent<Canvas>();
+        placePlayer2Screen = GameObject.Find("placePlayer2").GetComponent<Canvas>();
+        playingScreen = GameObject.Find("playingScreen").GetComponent<Canvas>();
         UpdateText();
         GameManager.OnPlayingStateChanged += UpdateOnPlayingStateChanged;
+
+        oldCanvas.gameObject.SetActive(false);
+        startScreen.gameObject.SetActive(true);
+        placeGroundScreen.gameObject.SetActive(false);
+        adjustFireScreen.gameObject.SetActive(false);
+        placePlayer1Screen.gameObject.SetActive(false);
+        placePlayer2Screen.gameObject.SetActive(false);
+        playingScreen.gameObject.SetActive(false);
     }
 
     private void UpdateText()
@@ -47,6 +71,31 @@ public class PlayUI : MonoBehaviour
             StartCoroutine("startBlinkButton");
         else
             StopCoroutine("startBlinkButton");
+    }
+
+    public void activatePlaceGround(){
+        switchScreen(startScreen, placeGroundScreen);
+    }
+
+    public void activateAdjustFireScreen(){
+        switchScreen(placeGroundScreen, adjustFireScreen);
+    }
+
+    public void activatePlacePlayer1() {
+        switchScreen(adjustFireScreen, placePlayer1Screen);
+    }
+
+    public void activatePlacePlayer2() {
+        switchScreen(placePlayer1Screen, placePlayer2Screen);
+    }
+
+    public void activatePlayingScreen() {
+        switchScreen(placePlayer2Screen, playingScreen);
+    }
+
+    private void switchScreen(Canvas a, Canvas b) {
+        a.gameObject.SetActive(false);
+        b.gameObject.SetActive(true);
     }
 
     private IEnumerator startBlinkButton()
