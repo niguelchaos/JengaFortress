1ae5f492e4db81b1f34a918a0c3a9d4c606ea0e6
Ibrahnab <ibrahim.naboulsi97@gmail.com>
2022-01-06 18:32:35 +0100

    small change

--- Assets/Scripts/AR/Setup.cs
+++ Assets/Scripts/AR/Setup.cs
@@ -77,8 +77,8 @@ public class Setup: MonoBehaviour
         // print("checking ui");
         if (GameManager.Instance.GetGameState() == GameState.SETUP)
         {
-            mainCanvas.SetActive(false);
-            setupCanvas.SetActive(true);
+            //mainCanvas.SetActive(false);
+            //setupCanvas.SetActive(true);
             switch (setupPhase)
             {
                 case SetupPhase.GROUNDPLANE:
--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -59,7 +59,7 @@ public class GameManager : MonoBehaviour
     {
         Instance = this;
         currentPlayer = CurrentPlayer.PLAYER_1;
-        SetGameState(GameState.SETUP);
+        SetGameState(GameState.MAIN_MENU);
         SetPlayingState(PlayingState.START_TURN);
     }
 
